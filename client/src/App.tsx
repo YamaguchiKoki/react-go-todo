199f9e7949c4398cbc87d38d17e410c99281ee9c
ko-ki <shankouhanghui15@gmail.com>
2024-09-17 17:11:56 +0900

    ad ui

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { Button, Container, Stack } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <>
+      <Stack h="100vh">
+        <NavBar />
+        <Container>
+          {/* <TodoForm />
+          <TodoList /> */}
+        </Container>
+
+      </Stack>
+    </>
+  )
+}
+
+export default App
