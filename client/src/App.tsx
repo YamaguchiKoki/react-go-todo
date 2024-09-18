d2b295e3fa213e0580e6fd0093fbb1a09482cac4
ko-ki <shankouhanghui15@gmail.com>
2024-09-18 19:05:56 +0900

    consume api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
 import { Button, Container, Stack } from '@chakra-ui/react'
+import TodoForm from './components/TodoForm'
+import Navbar from './components/Navbar'
+import TodoList from './components/TodoList'
 
+export const BASE_URL = "http://localhost:5001/api"
 function App() {
 
   return (
     <>
       <Stack h="100vh">
-        <NavBar />
+        <Navbar />
         <Container>
-          {/* <TodoForm />
-          <TodoList /> */}
+          <TodoForm />
+          <TodoList />
         </Container>
 
       </Stack>
